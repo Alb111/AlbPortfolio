097eb9152bcc7d68499f8f52c4eac301c50bc268
Alb111 <albertfelix2005@gmail.com>
2024-12-17 21:08:30 -0800

    hero section mostly done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,40 @@
+<script lang="ts">
+    import Typewriter from 'svelte-typewriter'
+</script>
+
+
 <main class="">
 	<section class="flex w-full flex-col sm:flex-row">
-		<div class="flex flex-col justify-center items-center w-full p-10 sm:w-1/2">
+		<div class="flex w-full flex-col items-center justify-center p-10 sm:w-1/2">
+ 				<h1 class="text-4xl font-bold bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent sm:text-7xl"
+					>Hello, Im
+				</h1>
+	           <Typewriter cursor={false} interval={50} mode=cascade>
 			<h1 class="text-4xl font-bold text-white sm:text-7xl">
-				<span class="bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent">Hello, Im
-				</span>
-				Albert Felix
+			Albert Felix
 			</h1>
-			<p class="text-xl py-10 text-center">
+</Typewriter>
+			<p class="py-10 text-center text-xl">
 				Im an apsiring hardware engineer, which intrests in ASIC design, AI, FGPAs, and Ditigal
 				System Design.
 			</p>
-			<button>Hire Me</button>
-			<button>Download CV</button>
+			<div class="flex space-x-4">
+				<!-- Hire Me Button -->
+				<button
+					class="border-gradient-to-r rounded-full border-2 from-blue-500 to-green-500 px-6 py-2 text-white transition duration-300 hover:bg-gradient-to-l "
+				>
+					Hire Me
+				</button>
+
+				<!-- Download CV Button -->
+				<button
+					class="border-gradient-to-r rounded-full border-2 from-blue-500 to-green-500 px-6 py-2 text-white transition duration-300 hover:bg-gradient-to-l "
+				>
+					Download CV
+				</button>
+			</div>
 		</div>
-		<div class="w-full  p-10 flex justify-center sm:w-1/2">
+		<div class="flex w-full justify-center p-10 sm:w-1/2">
 			<img class="rounded-xl" src="image.png" alt="akdjfalkdsjf;lk" />
 		</div>
 	</section>
