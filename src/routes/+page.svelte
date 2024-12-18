527ab0e34aff2b1fee44271792b238474d146c29
Alb111 <albertfelix2005@gmail.com>
2024-12-17 20:00:18 -0800

    got basic stuff going

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="">
+	<section class="flex w-full flex-col sm:flex-row">
+		<div class="flex flex-col justify-center items-center w-full p-10 sm:w-1/2">
+			<h1 class="text-4xl font-bold text-white sm:text-7xl">
+				<span class="bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent">Hello, Im
+				</span>
+				Albert Felix
+			</h1>
+			<p class="text-xl py-10 text-center">
+				Im an apsiring hardware engineer, which intrests in ASIC design, AI, FGPAs, and Ditigal
+				System Design.
+			</p>
+			<button>Hire Me</button>
+			<button>Download CV</button>
+		</div>
+		<div class="w-full  p-10 flex justify-center sm:w-1/2">
+			<img class="rounded-xl" src="image.png" alt="akdjfalkdsjf;lk" />
+		</div>
+	</section>
+</main>
