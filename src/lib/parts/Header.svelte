663d7b161cb5784f9cecd92db4546bd31dcb367e
Alb111 <albertfelix2005@gmail.com>
2025-01-08 16:15:22 -0800

    sidebar progress

--- src/lib/parts/Header.svelte
+++ src/lib/parts/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import SideNav from "./SideNav.svelte";
 	let x: number = 0;
 	function underline(y: number) {
 		x = y;
@@ -6,7 +7,7 @@
 </script>
 
 <div
-	class="sticky top-2 m-2 flex items-baseline justify-between rounded-xl bg-darkGray py-5 outline"
+	class="sticky top-2 m-2 flex items-baseline justify-between rounded-xl bg-darkGray py-2 sm:py-5 outline"
 >
 	<h1 class="ml-10 text-3xl">
 		Albert<span
@@ -14,20 +15,28 @@
 			>.</span
 		>
 	</h1>
-	<nav class="mr-10">
+
+	<!-- Main Navigation (visible on larger screens) -->
+	<nav class="hidden sm:flex mr-10">
 		<button class={x == 0 ? 'underlined' : 'normal'} on:click={() => underline(0)}>Home</button>
 		<button class={x == 1 ? 'underlined' : 'normal'} on:click={() => underline(1)}>Experience</button>
 		<button class={x == 2 ? 'underlined' : 'normal'} on:click={() => underline(2)}>Projects</button>
 		<button class={x == 3 ? 'underlined' : 'normal'} on:click={() => underline(3)}>Resume</button>
 		<button class={x == 4 ? 'underlined' : 'normal'} on:click={() => underline(4)}>Contact</button>
 	</nav>
+
+	<!-- Side Navigation (visible on small screens) -->
+	<div class="sm:hidden">
+		<SideNav/>
+	</div>
 </div>
+
 
 <style>
 	.normal {
 		@apply px-3 text-2xl;
 	}
 	.underlined {
-		@apply px-3 text-2xl underline;
+		@apply px-3 text-2xl bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text font-bold text-transparent;
 	}
 </style>
---
+++ src/lib/parts/SideNav.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Menu, CircleX  } from 'lucide-svelte';
+	// Set the width of the side navigation to 250px
+	function openNav() {
+		document.getElementById('mySidenav').classList.add('w-64');
+	}
+
+	// Set the width of the side navigation to 0
+	function closeNav() {
+		document.getElementById('mySidenav').classList.remove('w-64');
+	}
+</script>
+
+<div
+	id="mySidenav"
+	class="fixed right-0 top-0 z-10 h-full w-0 overflow-x-hidden bg-darkGray pt-16 transition-all duration-500"
+>
+	<button class="absolute left-6 top-0 text-3xl text-white" on:click={closeNav}> <CircleX/> </button>
+	<a href="#" class="block px-8 py-2 text-xl"
+		>About</a
+	>
+	<a href="#" class="block px-8 py-2 text-xl"
+		>Services</a
+	>
+	<a href="#" class="block px-8 py-2 text-xl"
+		>Clients</a
+	>
+	<a href="#" class="block px-8 py-2 text-xl"
+		>Contact</a
+	>
+</div>
+
+<!-- Use any element to open the sidenav -->
+<button on:click={openNav}>
+    <Menu class="mr-10"/>
+</button>
