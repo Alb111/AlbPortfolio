8fc6cf5c94e50b1282558fd2c952b9f78914abbe
Alb111 <albertfelix2005@gmail.com>
2025-01-18 14:56:52 -0800

    got some work done on projects seciton

--- src/lib/parts/Projects.svelte
+++ src/lib/parts/Projects.svelte
@@ -3,54 +3,75 @@
 	let projects = [
 		{
 			projectName: 'Pomato',
+			opensource: true,
 			githubUrl: 'https://github.com/Alb111/Pomato_V1',
-			description: `Pomato | Svelte Kit, Supabase, Tailwind-CSS, Typescript, Skeleton UI (Sept 2024 – Present)<br>
-• Developing a full-stack web application using Supabase and Svelte-Kit to help users implement the Pomodoro technique for more efficient task completion.<br>
-• Implementing OAuth to securely store user study sessions and track progress.<br>
-• Creating features to display past user data, study sessions, and completed tasks, providing progress tracking and motivation.`
+			skills: ['Svelte Kit', 'Supabase', 'Tailwind-CSS', 'Typescript', 'Skeleton UI'],
+			date: 'dec 2024',
+			description: [
+				'• Developing a full-stack web application using Supabase and Svelte-Kit to help users implement the Pomodoro technique for more efficient task completion',
+				'• Implementing OAuth to securely store user study sessions and track progress',
+				'• Creating features to display past user data, study sessions, and completed tasks, providing progress tracking and motivation'
+			],
+			img: ''
 		},
 		{
 			projectName: 'Slug Assistant *Cruz Hacks Winner*',
+			opensource: true,
 			githubUrl: 'https://github.com/Sluggish-Solutions/Slug-Mommy',
-			description: `Slug Assistant *Cruz Hacks Winner* | Svelte Kit, Supabase, Tailwind-CSS, Typescript, Skeleton UI (Feb 2024)<br>
-• Lead front-end development to create a school mascot-themed AI assistant to help incoming college students better handle their daily schedules and responsibilities.<br>
-• Effectively implemented OpenAI’s API and simple prompting to facilitate a welcoming tone of communication.<br>
-• Repo: https://github.com/Sluggish-Solutions/Slug-Mommy<br>
-• Devpost: https://devpost.com/software/slug-mommy`
+			skills: ['Svelte Kit', 'Supabase', 'Tailwind-CSS', 'Typescript', 'Skeleton UI'],
+			date: 'feb 2024',
+			description: [
+				'• Lead front-end development to create a school mascot-themed AI assistant to help incoming college students better handle their daily schedules and responsibilities',
+				'• Effectively implemented OpenAI’s API and simple prompting to facilitate a welcoming tone of communication',
+				'• Devpost: https://devpost.com/software/slug-mommy'
+			]
 		},
 		{
 			projectName: 'Stop it Game',
+			opensource: false,
 			githubUrl: 'Can be shown on request currently private for academic purposes.',
-			description: `Stop It Game | Verilog, FPGA, Verible, Vivado (July 2024)<br>
-• Developed a real-time timing game, Stop It, on an FPGA using Verilog, involving precise timing and control logic.<br>
-• Implemented digital logic circuits to handle game state, timing, score, and user input for start/stop functionality on an LED display and a set of score LEDs.<br>
-• Tested and debugged the system on FPGA hardware, ensuring reliable timing precision and responsive gameplay.`
+			skills: ['Verilog', 'FPGA', 'Verible', 'Vivado'],
+			date: 'July 2024',
+			description: [
+				'• Developed a real-time timing game, Stop It, on an FPGA using Verilog, involving precise timing and control logic',
+				'• Implemented digital logic circuits to handle game state, timing, score, and user input for start/stop functionality on an LED display and a set of score LEDs',
+				'• Tested and debugged the system on FPGA hardware, ensuring reliable timing precision and responsive gameplay'
+			]
 		},
 		{
 			projectName: 'CLI-based Hangman',
+			opensource: false,
+			skills: ['C', 'Make'],
 			githubUrl: 'Can be shown on request currently private for academic purposes.',
-			description: `CLI-based Hangman | C (May 2024)<br>
-• Developed a command-line interface (CLI) based Hangman game with ASCII art, providing a visually engaging user experience.<br>
-• Implemented game logic for word selection, tracking player guesses, and managing game states such as wins/losses.<br>
-• Created a robust input validation system to handle incorrect guesses and provide real-time feedback.`
+			date: 'Mar 2024',
+			description: [
+				'• Developed a command-line interface (CLI) based Hangman game with ASCII art, providing a visually engaging user experience',
+				'• Implemented game logic for word selection, tracking player guesses, and managing game states such as wins/losses',
+				'• Created a robust input validation system to handle incorrect guesses and provide real-time feedback'
+			]
 		}
 	];
 
+	/**
+	 * @param {number} n
+	 */
 	function plusSlides(n) {
 		slideIndex = ((slideIndex + n - 1 + projects.length) % projects.length) + 1;
 	}
 
+	/**
+	 * @param {number} n
+	 */
 	function currentSlide(n) {
 		slideIndex = n;
 	}
 </script>
 
-
-<div class="max-w-screen px-10">
-	<div class="w-full">
+<div class="h-1/2 w-full">
+	<div class="w-1/2 h-full flex items-center">
 		{#each projects as project, index}
 			<div class={`mySlides fade ${index + 1 === slideIndex ? 'block' : 'hidden'}`}>
-				<div class="h-52 rounded-lg bg-lightGray p-6">
+				<div class="rounded-lg bg-lightGray p-6">
 					<h3
 						class="bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-xl font-bold text-transparent"
 					>
@@ -61,12 +82,19 @@
 							GitHub Link
 						</a>
 					</p>
-					<p class="mt-3 text-white">{project.description}</p>
+					{#each project.description as bulletpoint}
+						<p class="mt-3 text-white">{bulletpoint}</p>
+					{/each}
 				</div>
 			</div>
 		{/each}
+	</div>
 
-		<!-- Next & previous buttons -->
+
+
+</div>
+
+	<!-- Next & previous buttons -->
 		<button
 			class=" rounded-r bg-black bg-opacity-50 px-4 py-2 text-lg font-bold text-white hover:bg-opacity-70"
 			on:click={() => plusSlides(-1)}
@@ -79,8 +107,6 @@
 		>
 			→
 		</button>
-	</div>
-</div>
 
 <style>
 	/* Fading animation for the slides */
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
 	import Hero from "$lib/parts/Hero.svelte";
 	import Header from "$lib/parts/Header.svelte";
 	import WorkExp from "$lib/parts/WorkExperience.svelte";
-	import Education from "$lib/parts/Education.svelte";
 	import Projects from "$lib/parts/Projects.svelte";
 </script>
 
 <Header/>
 <Hero/>
 <WorkExp/>
+<Projects/>
